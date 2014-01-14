c2219a64cdb22f3a39d9dd96fac3cbec8fbde1d6
Carsten König <Carsten@gettingsharper.de>
2014-01-14 12:05:16 +0100

    some refactoring and now using static start image

--- Mandelbrot/main.hs
+++ Mandelbrot/main.hs
@@ -3,6 +3,7 @@ module Main where
 import Mandelbrot
 
 import Control.Monad
+import System.Directory (doesFileExist)
 
 import qualified Graphics.UI.Threepenny as UI
 import qualified Graphics.UI.Threepenny.Attributes as Attr
@@ -10,27 +11,54 @@ import qualified Graphics.UI.Threepenny.Events as Ev
 
 import Graphics.UI.Threepenny.Core
 
-main :: IO ()
-main = do
-    startGUI defaultConfig
-        { tpPort = 10000
-        , tpStatic = Just "./"
-        } setup
+data Settings = Settings
+    { resolution  :: PictureSize
+    , zoom        :: Double
+    , renderSteps :: Steps}
 
-setup :: Window -> UI ()
-setup w = void $ do
-        return w # set title "Mandelbrot generator "
-        -- UI.addStyleSheet w "buttons.css"
-
-        (md, start) <- mandelbrotDisplay defaultView defaultRes
-                        defaultSteps defaultZoom
-        getBody w #+ [element md]
-        start
+defaultSettings :: Settings
+defaultSettings = Settings defaultRes defaultZoom defaultSteps
     where
-    defaultView    = View (C (-2.4) 1.2) (C 1.1 (-1.4))
     defaultRes     = Size 1024 711
     defaultSteps   = 1024
     defaultZoom    = 4
+
+startView :: ViewWindow
+startView          = View (C (-2.4) 1.2) (C 1.1 (-1.4))
+
+startImageName :: String
+startImageName = "start.png"
+
+renderImageName :: String
+renderImageName = "mandelbrot.png"
+
+localPath :: String -> FilePath
+localPath name = "./" ++ name
+
+clientPath :: String -> String
+clientPath name = "/static/" ++ name
+
+main :: IO ()
+main = do
+
+    let settings = defaultSettings
+
+    -- create the start-image if neccessary
+    verifyStartImageFile settings $ localPath startImageName
+
+    putStrLn "starting local server..."
+    startGUI defaultConfig
+        { tpPort = 10000
+        , tpStatic = Just "./"
+        } (setup settings)
+
+setup :: Settings -> Window -> UI ()
+setup settings w = void $ do
+        return w # set title "Mandelbrot generator "
+        -- UI.addStyleSheet w "buttons.css"
+
+        md <- mandelbrotDisplay settings
+        getBody w #+ [element md]
 
 data MandelbrotDisplay = MandelbrotDisplay
     { view       :: Behavior ViewWindow
@@ -40,29 +68,41 @@ data MandelbrotDisplay = MandelbrotDisplay
 instance Widget MandelbrotDisplay where
     getElement = visual    
 
-mandelbrotDisplay :: ViewWindow -> PictureSize -> Steps -> Double -> UI (MandelbrotDisplay, UI ())
-mandelbrotDisplay startView res maxSteps zoomF = do
-    img <- mkImage ""
+mandelbrotDisplay :: Settings -> UI MandelbrotDisplay
+mandelbrotDisplay settings = do
+    img <- mkImage
     msg <- string ""
     d   <- UI.div #. "wrap" #+ [element msg, element img]
 
     let
-        makeViewChange (ptX, ptY) vw = zoomTo zoomF cent vw
-            where cent = project vw res (Coords ptX ptY)
+        makeViewChange (ptX, ptY) vw = zoomTo (zoom settings) cent vw
+            where cent = project vw (resolution settings) (Coords ptX ptY)
 
         renderImage viewVal = void $ do
             element img # set Attr.src ""
             element msg # set text "rendering please wait ..."
-            liftIO $ createImageParallel viewVal maxSteps res "./mandelbrot.png"
+            liftIO $ renderView settings viewVal (localPath renderImageName)
             element msg # set text ""
-            element img # set Attr.src "/static/mandelbrot.png"
+            element img # set Attr.src (clientPath renderImageName)
 
     vw <- accumB startView $ makeViewChange <$> Ev.mousedown img
     onChanges vw renderImage
 
-    return $ (MandelbrotDisplay vw d, renderImage startView)
+    return $ (MandelbrotDisplay vw d)
 
-mkImage :: String -> UI Element
-mkImage source = do
+mkImage :: UI Element
+mkImage = do
     img <- UI.image #. "mandelImage"
-    element img # set Attr.src source
+    element img # set Attr.src (clientPath startImageName)
+
+verifyStartImageFile :: Settings -> FilePath -> IO()
+verifyStartImageFile settings filePath = do
+    startExist <- doesFileExist filePath
+    when (not startExist) $ do
+        putStrLn "creating starting image - one moment please..."
+        renderView settings startView filePath
+        putStrLn "done"
+
+renderView :: Settings -> ViewWindow -> FilePath -> IO()
+renderView settings vw = createImageParallel vw (renderSteps settings) (resolution settings)
+
